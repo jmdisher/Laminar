162be827c3af690914d5e15863606b1b2a26496b
Jeff Disher <jeff@jeffdisher.com>
2020-05-25 15:32:57 -0400

    Added a "chat server" example to demonstrate Laminar basics
    -this tool can be used directly to demonstrate how the cluster can be used and what data movement across the cluster means

---
+++ integration/src/com/jeffdisher/laminar/examples/ChatServerExample.java
@@ -0,0 +1,102 @@
+package com.jeffdisher.laminar.examples;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+import com.jeffdisher.laminar.client.ClientConnection;
+import com.jeffdisher.laminar.client.ListenerConnection;
+import com.jeffdisher.laminar.types.CommitInfo;
+import com.jeffdisher.laminar.types.TopicName;
+import com.jeffdisher.laminar.types.event.EventRecord;
+import com.jeffdisher.laminar.types.event.EventRecordPayload_Put;
+import com.jeffdisher.laminar.types.event.EventRecordType;
+import com.jeffdisher.laminar.utils.Assert;
+
+
+/**
+ * An interactive client example to demonstrate basics of Laminar and make its actions observable.
+ * Starts a client and listener, internally, using a topic name as a chat room.
+ * Lines submitted on STDIN will be posted to the topic and anything the listener observes will be written to STDOUT
+ * with the UUID of the client who posted it.
+ * There is no active effort to avoid these 2 operations "fighting over the console" as this is just a basic example,
+ * not a real application.
+ * 
+ * Args (just as a list):
+ * -ip - the IP of one of the nodes in the cluster
+ * -port - the client port of that node
+ * -topic - the chat room name
+ * -name - The name the user should use to identity themselves in the room.
+ */
+public class ChatServerExample {
+	public static void main(String[] args) throws Throwable {
+		if (4 != args.length) {
+			_usageAndExit();
+		}
+		InetAddress ip = InetAddress.getByName(args[0]);
+		int port = Integer.parseInt(args[1]);
+		InetSocketAddress address = new InetSocketAddress(ip, port);
+		TopicName topicName = TopicName.fromString(args[2]);
+		String userName = args[3];
+		System.out.print(userName + " connecting to: " + address + " on topic: " + topicName + "...");
+		
+		ClientConnection client = ClientConnection.open(address);
+		ListenerConnection listener = ListenerConnection.open(address, topicName, 0L);
+		client.waitForConnectionOrFailure();
+		listener.waitForConnectionOrFailure();
+		System.out.println(" Connected!");
+		System.out.println("Type \"stop\" to exit.");
+		Thread listenerThread = new Thread(() -> {
+			try {
+				EventRecord record = listener.pollForNextEvent();
+				while (null != record) {
+					// If this is a put, write it to STDOUT.
+					if (EventRecordType.PUT == record.type) {
+						String name = new String(((EventRecordPayload_Put) record.payload).key, StandardCharsets.UTF_8);
+						String text = new String(((EventRecordPayload_Put) record.payload).value, StandardCharsets.UTF_8);
+						System.out.println(name + ": " + text);
+					}
+					record = listener.pollForNextEvent();
+				}
+			} catch (InterruptedException e) {
+				// We don't use interruption.
+				throw Assert.unexpected(e);
+			}
+		});
+		listenerThread.start();
+		
+		// Start by making sure that there is a topic for the room.
+		CommitInfo createInfo = client.sendCreateTopic(topicName).waitForCommitted();
+		if (CommitInfo.Effect.VALID == createInfo.effect) {
+			System.out.println("(room created)");
+		}
+		
+		byte[] rawUser = userName.getBytes(StandardCharsets.UTF_8);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		boolean keepRunning = true;
+		while (keepRunning) {
+			String line = reader.readLine();
+			if ("stop".equals(line)) {
+				keepRunning = false;
+			} else {
+				byte[] rawLine = line.getBytes(StandardCharsets.UTF_8);
+				CommitInfo info = client.sendPut(topicName, rawUser, rawLine).waitForCommitted();
+				if (CommitInfo.Effect.VALID != info.effect) {
+					System.err.println("INVALID COMMIT!");
+				}
+			}
+		}
+		
+		client.close();
+		listener.close();
+		listenerThread.join();
+	}
+
+
+	private static void _usageAndExit() {
+		System.err.println("Usage: ChatServerExample <ip> <port> <chatroom> <username>");
+		System.exit(1);
+	}
+}
---
+++ integration/test/com/jeffdisher/laminar/examples/TestChatServerExample.java
@@ -0,0 +1,77 @@
+package com.jeffdisher.laminar.examples;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jeffdisher.laminar.ProcessWrapper;
+import com.jeffdisher.laminar.ServerWrapper;
+
+
+/**
+ * Integration test of the ChatServerExample.
+ * This requires env vars to be set:
+ * -WRAPPER_SERVER_JAR - points to the JAR of the complete Laminar server
+ * -CHAT_CLIENT_JAR - points to the JAR of the ChatServerExample
+ */
+public class TestChatServerExample {
+	/**
+	 * Starts 1 node and attaches 2 clients to it, verifying that the first sees the topic created and that both see
+	 * each other speaking.
+	 */
+	@Test
+	public void test2Clients() throws Throwable {
+		String roomName = "chatroom";
+		ServerWrapper leader = ServerWrapper.startedServerWrapper("test2Clients-LEADER", 2001, 3001, new File("/tmp/laminar1"));
+		InetSocketAddress leaderAddress = new InetSocketAddress(InetAddress.getLocalHost(), 3001);
+		
+		// (HACK: we need to wait for the servers to start up).
+		Thread.sleep(500);
+		ProcessWrapper user1 = _startChatClient(leaderAddress.getAddress().getHostAddress(), Integer.toString(leaderAddress.getPort()), roomName, "user1");
+		CountDownLatch roomCreation = user1.filterStdout("(room created)");
+		user1.startFiltering();
+		roomCreation.await();
+		ProcessWrapper user2 = _startChatClient(leaderAddress.getAddress().getHostAddress(), Integer.toString(leaderAddress.getPort()), roomName, "user2");
+		user2.startFiltering();
+		
+		// Both clients should see the other message and their own.
+		CountDownLatch fromUser1 = user2.filterStdout("user1: Hello");
+		CountDownLatch echoUser1 = user1.filterStdout("user1: Hello");
+		CountDownLatch fromUser2 = user1.filterStdout("user2: I see you");
+		CountDownLatch echoUser2 = user2.filterStdout("user2: I see you");
+		
+		// Send both messages.
+		user1.sendStdin("Hello");
+		user2.sendStdin("I see you");
+		
+		// Wait for everything.
+		fromUser1.await();
+		echoUser1.await();
+		fromUser2.await();
+		echoUser2.await();
+		
+		// Shut down.
+		user1.sendStdin("stop");
+		user2.sendStdin("stop");
+		Assert.assertEquals(0, user1.waitForTermination());
+		Assert.assertEquals(0, user2.waitForTermination());
+		Assert.assertEquals(0, leader.stop());
+	}
+
+
+	private static ProcessWrapper _startChatClient(String... mainArgs) throws Throwable {
+		String jarPath = System.getenv("CHAT_CLIENT_JAR");
+		if (null == jarPath) {
+			throw new IllegalArgumentException("Missing CHAT_CLIENT_JAR env var");
+		}
+		if (!new File(jarPath).exists()) {
+			throw new IllegalArgumentException("JAR \"" + jarPath + "\" doesn't exist");
+		}
+		
+		return ProcessWrapper.startedJavaProcess(jarPath, mainArgs);
+	}
+}
